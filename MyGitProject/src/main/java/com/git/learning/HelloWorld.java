5b9ee2b4841d8ebdd59fc0cbc1b3e5a83a57d8f0
MBTech <musabaytechcorp@gmail.com>
2020-12-26 20:35:52 +0000

    deleted auto message, added hello world print line

--- MyGitProject/src/main/java/com/git/learning/HelloWorld.java
+++ MyGitProject/src/main/java/com/git/learning/HelloWorld.java
@@ -3,7 +3,7 @@ package com.git.learning;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
 
 	}
 
